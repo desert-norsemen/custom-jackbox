3e7c64f6929538e732add70145219bd11db295d5
Benston <beverage13@gmail.com>
2018-06-29 22:21:34 -0600

    The old program, but MUCH better.

--- smile.py
+++ smile.py
@@ -1,6 +1,35 @@
 smile = ':)'
 matt = 'Matthew Michael Morris'
+jordan = 'Jordan Daniel Cooper'
+taylor = 'Taylor f@#*&$&% Earl'
+ben = 'Benjamin Bud Beveridge'
+stetson = 'yikes'
+
 for c in matt:
+    if c != ' ':
+        print(smile, end='')
+    else:
+        print('    ', end='')
+
+for c in jordan:
+    if c != ' ':
+        print(smile, end='')
+    else:
+        print('    ', end='')
+
+for c in taylor:
+    if c != ' ':
+        print(smile, end='')
+    else:
+        print('    ', end='')
+
+for c in ben:
+    if c != ' ':
+        print(smile, end='')
+    else:
+        print('    ', end='')
+
+for c in stetson:
     if c != ' ':
         print(smile, end='')
     else:
